3835dfd30367d82d00cba771ce652ba26c102f2b
Aleksandr Rodionov <alexrod93@yandex.ru>
2024-04-11 01:54:55 +0300

    zhopa

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+//import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,13 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <p>zhi est</p>
         </a>
       </header>
     </div>
